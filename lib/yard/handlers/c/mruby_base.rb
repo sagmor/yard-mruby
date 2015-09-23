3474e63884bc7fb8dbacd2c94a5b30f6abf155c8
Seba Gamboa <me@sagmor.com>
2015-09-23 15:25:53 -0300

    Register default types

--- lib/yard/handlers/c/mruby_base.rb
+++ lib/yard/handlers/c/mruby_base.rb
@@ -1,14 +1,28 @@
 module YARD::Handlers::C
   class MRubyBase < Base
+
     DEFAULT_NAMESPACES = {
-      'mrb->object_class' => 'Object'
+      # 'mrb->top_self'      => '',
+      'mrb->object_class'  => 'Object',
+      'mrb->class_class'   => 'Class',
+      'mrb->module_class'  => 'Module',
+      'mrb->proc_class'    => 'Proc',
+      'mrb->string_class'  => 'String',
+      'mrb->array_class'   => 'Array',
+      'mrb->hash_class'    => 'Hash',
+      'mrb->float_class'   => 'Float',
+      'mrb->fixnum_class'  => 'Fixnum',
+      'mrb->true_class'    => 'TrueClass',
+      'mrb->false_class'   => 'FalseClass',
+      'mrb->nil_class'     => 'NilClass',
+      'mrb->symbol_class'  => 'Symbol',
+      'mrb->kernel_module' => 'Kernel'
     }
 
     def namespace_for_variable(var)
       return DEFAULT_NAMESPACES[var] if DEFAULT_NAMESPACES[var]
       super
     end
-
 
   end
 end
